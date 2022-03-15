185ee9e931145da6010dd59eeaf440fe34e11c5a
Tanner253 <PercivalTanner@gmail.com>
2022-03-15 11:44:07 -0700

    adds pfp

--- src/modules/About.js
+++ src/modules/About.js
@@ -5,10 +5,10 @@ export default class About extends Component {
   render() {
     return (
       <>
-      <Container className="text-center">
+      <Container className="text-center mt-4">
         <Col>
         <Row xs={1} sm={1} md={2} lg={2}>
-        <Card style={{ width: "35rem" }}>
+        <Card style={{ width: "50%" }}>
           <Card.Img variant="top" src="./images/headshot.jpg" />
           <Card.Body>
             <Card.Title>Marian Anderson</Card.Title>
@@ -18,7 +18,7 @@ export default class About extends Component {
             <Button href="https://www.vagaro.com/hairbymarian">Book Me!</Button>
           </Card.Body>
         </Card>
-        <Card style={{ width: "35rem" }}>
+        <Card style={{ width: "50%" }}>
           <Card.Img variant="top" src="./images/ID.PNG" />
           <Card.Body>
             <Card.Title>Tanner Percival - Developer</Card.Title>
